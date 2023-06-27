87223d5443aa8fd46aa77f516c27d6e97d68ba40
Vostenznuk <dimacontacto94@gmail.com>
2023-06-27 19:17:15 +0200

    agregar archivos

---
+++ www/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="css/estilos.css">
+    <title>PHP</title>
+</head>
+
+<body>
+    <h1> Actividades PHP </h1>
+        <?php
+        for ($i = 1; $i < 10; $i++) {
+                echo "Hola Mundo";
+                echo "<br>";
+        }
+        ?>
+</body>
+
+</html>
